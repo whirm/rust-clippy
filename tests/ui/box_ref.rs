bf1e5c9422ea71bc71e9ea78135e1bfbe99e8f56
Félix Fischer <felix91gr@gmail.com>
2025-05-16 17:16:49 +0200

    Tidy up the test file

--- tests/ui/box_ref.rs
+++ tests/ui/box_ref.rs
@@ -1,17 +1,36 @@
 #![warn(clippy::box_ref)]
 
-fn main() {
-    let ok1 = "";
-    let ok2 = Box::new(&ok1);
+fn main() {}
 
+fn string_slice_is_ok_to_box() {
+    let the_slice = "";
+    let the_ok = Box::new(&the_slice);
+}
+
+fn reference_to_a_literal_is_redundant_to_box() {
     let a = 1u32;
 
     let b = Box::new(&a);
-    //~^ box_ref
+}
+
+fn type_annotations_of_a_boxed_int_is_redundant_to_box() {
+    let a = 1u32;
+
+    #[allow(clippy::box_ref)]
+    let b = Box::new(&a);
 
     let c: Box<&u32> = b;
-    //~^ box_ref
-
-    let d: Box<&u32> = (&a).into();
-    //~^ box_ref
 }
+
+// TODO if I remember how to do into() without the type annotation
+// fn four() {
+//     let a = 1u32;
+
+//     #[allow(clippy::box_ref)]
+//     let b = Box::new(&a);
+
+//     #[allow(clippy::box_ref)]
+//     let c: Box<&u32> = b;
+
+//     let d = (&a).into();
+// }
