1dda1dee48712c46c1c2ff3a5c3cbcc228de11d3
Elric Milon <git@whirm.eu>
2025-05-23 11:07:46 +0200

    fixup! Tidy up the test file

--- tests/ui/box_ref.rs
+++ tests/ui/box_ref.rs
@@ -11,6 +11,7 @@ fn reference_to_a_literal_is_redundant_to_box() {
     let a = 1u32;
 
     let b = Box::new(&a);
+    //~^ box_ref
 }
 
 fn type_annotations_of_a_boxed_int_is_redundant_to_box() {
@@ -20,17 +21,21 @@ fn type_annotations_of_a_boxed_int_is_redundant_to_box() {
     let b = Box::new(&a);
 
     let c: Box<&u32> = b;
+    //~^ box_ref
 }
 
-// TODO if I remember how to do into() without the type annotation
-// fn four() {
-//     let a = 1u32;
+fn box_from_thin_ref_is_redundant_to_box() {
+    let a = 1u32;
 
-//     #[allow(clippy::box_ref)]
-//     let b = Box::new(&a);
+    let b = Box::from(&a);
+    //~^ box_ref
+}
 
-//     #[allow(clippy::box_ref)]
-//     let c: Box<&u32> = b;
-
-//     let d = (&a).into();
-// }
+// TODO:
+// From https://github.com/rust-lang/rust-clippy/issues/2394
+// Box<&T>
+// Box::new(SomeT) where sizeof::<T>() <= sizeof::<usize>()
+// unless there are Box::into_raw calls within the function
+// Rc<Box<T>>
+// Rc<Rc<T>>
+// Rc<&T>
