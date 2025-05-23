25a3bb891601935b2ba63b06300442fd5b5bb371
Elric Milon <git@whirm.eu>
2025-05-23 11:07:46 +0200

    fixup! WIP: box_ref lint

--- clippy_lints/src/box_ref.rs
+++ clippy_lints/src/box_ref.rs
@@ -1,9 +1,7 @@
-use std::any::Any;
-
 use clippy_utils::diagnostics::{span_lint, span_lint_and_sugg};
 use clippy_utils::source::snippet;
 use rustc_errors::Applicability;
-use rustc_hir::{Expr, *};
+use rustc_hir::*;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::declare_lint_pass;
 
@@ -30,45 +28,40 @@ declare_lint_pass!(BoxRef => [BOX_REF]);
 
 impl LateLintPass<'_> for BoxRef {
     //This works but doesn't span the whole expression
-    fn check_ty<'tcx>(&mut self, cx: &LateContext<'tcx>, ty: &Ty<'tcx, AmbigArg>) {
-        let ty_n = clippy_utils::ty::ty_from_hir_ty(cx, ty.as_unambig_ty());
+    fn check_ty<'tcx>(&mut self, cx: &LateContext<'tcx>, hir_ty: &Ty<'tcx, AmbigArg>) {
+        let ty = clippy_utils::ty::ty_from_hir_ty(cx, hir_ty.as_unambig_ty());
 
-        if let Some(boxed_ty) = ty_n.boxed_ty()
-            && boxed_ty.is_ref()
-            && boxed_ty.peel_refs().is_sized(cx.tcx, cx.typing_env())
-        {
-            span_lint(cx, BOX_REF, ty.span, "TODO: lint msg");
+        if is_boxed_sized_ref(cx, ty) {
+            span_lint(cx, BOX_REF, hir_ty.span, "TODO: lint msg");
         }
     }
 
     fn check_expr(&mut self, cx: &LateContext<'_>, expr: &'_ Expr<'_>) {
         // Get type of `expr`
         let ty = cx.typeck_results().expr_ty(expr);
-        if let Some(boxed_ty) = ty.boxed_ty()
-            && boxed_ty.is_ref()
-        {
-            match expr.kind {
-                ExprKind::Call(_, &[Expr { span, .. }]) => {
-                    span_lint_and_sugg(
-                        cx,
-                        BOX_REF,
-                        expr.span,
-                        "TODO: lint msg2",
-                        "Remove Box<>",
-                        format!("{}", snippet(cx, span, "<default>")),
-                        Applicability::MachineApplicable,
-                    );
-                },
-                _ => (),
+
+        if is_boxed_sized_ref(cx, ty) {
+            if let ExprKind::Call(_, &[Expr { span, .. }]) = expr.kind {
+                span_lint_and_sugg(
+                    cx,
+                    BOX_REF,
+                    expr.span,
+                    "TODO: lint msg2",
+                    "Remove Box",
+                    format!("{}", snippet(cx, span, "<default>")),
+                    Applicability::MachineApplicable,
+                );
             };
         }
-        // // Match its kind to enter its type
-        // if ty.boxed_ty().is_some_and(|x| x.is_ref()) {
-        //     // span_lint(cx, BOX_REF, expr.span, "Ooops");
-        // };
-        // match ty.kind() {
-        //     ty::Adt(adt_def, _) if adt_def.is_struct() => println!("Our `expr` is a struct!"),
-        //     _ => (),
-        // }
+    }
+}
+
+fn is_boxed_sized_ref<'tcx>(cx: &LateContext<'tcx>, ty: rustc_middle::ty::Ty<'tcx>) -> bool {
+    if let Some(boxed_ty) = ty.boxed_ty()
+        && boxed_ty.is_ref()
+    {
+        boxed_ty.peel_refs().is_sized(cx.tcx, cx.typing_env())
+    } else {
+        false
     }
 }
